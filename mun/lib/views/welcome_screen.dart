ed75afd858e1bc0ef84ee81f786d440b41578232
smit-mist <smit3355mistry@gmail.com>
2020-12-25 07:21:50 +0530

    Theme set and views directory created

---
+++ mun/lib/views/welcome_screen.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class WelcomeScreen extends StatefulWidget {
+  @override
+  _WelcomeScreenState createState() => _WelcomeScreenState();
+}
+
+class _WelcomeScreenState extends State<WelcomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    double h = MediaQuery.of(context).size.height;
+    double w = MediaQuery.of(context).size.width;
+    return Scaffold(
+      appBar: AppBar(
+        title: Center(
+          child: Text(
+            "THE MUN",
+          ),
+        ),
+      ),
+      body: Container(),
+    );
+  }
+}
