a6ccaf3c54c1e88717ea5ac5df2b0df6ba39d60a
smit-mist <smit3355mistry@gmail.com>
2021-01-11 17:50:15 +0530

    Fonts added to assets and sign up page almost completed

--- mun/lib/views/signup_screen.dart
+++ mun/lib/views/signup_screen.dart
@@ -15,154 +15,219 @@ class _SignUpScreenState extends State<SignUpScreen> {
     double h = MediaQuery.of(context).size.height;
     double w = MediaQuery.of(context).size.width;
     return Scaffold(
-      appBar: AppBar(
-        title: Text(' '),
-      ),
       body: Center(
         child: SingleChildScrollView(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              Text(
-                'Sign Up',
-                style: Theme.of(context).textTheme.headline6,
-              ),
-              SizedBox(
-                height: h * 0.05,
-              ),
-              Container(
-                width: w * 0.85,
-                height: h * 0.6,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(20),
-                  border: Border.all(color: Colors.black),
+          child: Padding(
+            padding: const EdgeInsets.all(30.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Center(
+                  child: Text(
+                    'BookMyMun',
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontWeight: FontWeight.bold,
+                      fontFamily: 'Helvetica',
+                      fontSize: 35,
+                    ),
+                  ),
                 ),
-                child: Center(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: [
-                      Container(
-                        width: w * 0.6,
-                        child: TextField(
-                          onChanged: (change) {
-                            setState(() {
-                              name = change;
-                            });
-                          },
-                          decoration: InputDecoration(
-                            labelText: 'Name',
-                            prefixIcon: Icon(Icons.account_circle),
-                          ),
-                        ),
-                      ),
-                      Container(
-                        width: w * 0.6,
-                        child: TextField(
-                          onChanged: (change) {
-                            setState(() {
-                              email = change;
-                            });
-                          },
-                          decoration: InputDecoration(
-                            labelText: 'Email',
-                            prefixIcon: Icon(Icons.email),
-                          ),
-                        ),
-                      ),
-                      Container(
-                        width: w * 0.6,
-                        child: TextField(
-                          onChanged: (change) {
-                            setState(() {
-                              password1 = change;
-                            });
-                          },
-                          obscureText: true,
-                          decoration: InputDecoration(
-                            labelText: 'Password',
-                            prefixIcon: Icon(Icons.vpn_key),
-                          ),
-                        ),
-                      ),
-                      Container(
-                        width: w * 0.6,
-                        child: TextField(
-                          onChanged: (change) {
-                            setState(() {
-                              password2 = change;
-                            });
-                          },
-                          obscureText: true,
-                          decoration: InputDecoration(
-                            labelText: 'Verify Password',
-                            prefixIcon: Icon(Icons.vpn_key_sharp),
-                          ),
-                        ),
-                      ),
-                      Column(
-                        children: [
-                          ElevatedButton(
-                            onPressed: () async {
+                SizedBox(
+                  height: h * 0.05,
+                ),
+                Text(
+                  'Create account',
+                  style: TextStyle(
+                    fontSize: 25,
+                    fontFamily: 'Helvetica',
+                  ),
+                ),
+                Container(
+                  width: w,
+                  height: h * 0.45,
+                  child: Center(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        Container(
+                          child: TextField(
+                            onChanged: (change) {
                               setState(() {
-                                isLoading = true;
+                                name = change;
                               });
-                              try {
-                                print('=============> here');
-                                final User user = (await _auth.createUserWithEmailAndPassword(
-                                        email: email, password: password1))
-                                    .user;
-                                setState(() {
-                                  isLoading = false;
-                                });
-                                Navigator.pushNamedAndRemoveUntil(
-                                    context, 'home', (route) => false);
-                              } catch (e) {
-                                print('==============>>');
-                                print(e);
-                                Scaffold.of(context).showSnackBar(
-                                  SnackBar(
-                                    content: Text('Something went wrong'),
-                                  ),
-                                );
-                              }
                             },
-                            child: Container(
-                              width: w * 0.45,
-                              child: Center(
-                                child: Text(
-                                  'Sign Up',
+                            decoration: InputDecoration(
+                              hintStyle: TextStyle(
+                                fontFamily: 'Helvetica',
+                                fontSize: 18,
+                              ),
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(3),
+                                borderSide: BorderSide(
+                                  width: 0,
+                                  style: BorderStyle.none,
+                                ),
+                              ),
+                              hintText: 'Name',
+                              contentPadding: EdgeInsets.symmetric(
+                                  horizontal: 10, vertical: 2),
+                              filled: true,
+                              fillColor: Colors.grey.withOpacity(0.25),
+                              focusColor: Colors.grey,
+                            ),
+                            cursorColor: Colors.black,
+                          ),
+                        ),
+                        Container(
+                          child: TextField(
+                            cursorColor: Colors.black,
+
+                            onChanged: (change) {
+                              setState(() {
+                                email = change;
+                              });
+                            },
+                            decoration: InputDecoration(
+                              hintText: 'Your email address',
+                              contentPadding: EdgeInsets.symmetric(
+                                  horizontal: 10, vertical: 2),
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(3),
+                                borderSide: BorderSide(
+                                  width: 0,
+                                  style: BorderStyle.none,
+                                ),
+                              ),
+                              filled: true,
+                              fillColor: Colors.grey.withOpacity(0.25),
+                              focusColor: Colors.grey,
+                            ),
+                          ),
+                        ),
+                        Container(
+                          child: TextField(
+                            cursorColor: Colors.black,
+
+                            onChanged: (change) {
+                              setState(() {
+                                password1 = change;
+                              });
+                            },
+                            obscureText: true,
+                            decoration: InputDecoration(
+                              hintText: 'Password',
+                              contentPadding: EdgeInsets.symmetric(
+                                  horizontal: 10, vertical: 2),
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(3),
+                                borderSide: BorderSide(
+                                  width: 0,
+                                  style: BorderStyle.none,
+                                ),
+                              ),
+                              filled: true,
+                              fillColor: Colors.grey.withOpacity(0.25),
+                              focusColor: Colors.grey,
+                            ),
+                          ),
+                        ),
+                        Container(
+                          child: TextField(
+                            cursorColor: Colors.black,
+
+                            onChanged: (change) {
+                              setState(() {
+                                password2 = change;
+                              });
+                            },
+                            obscureText: true,
+                            decoration: InputDecoration(
+                              hintText: 'Verify Password',
+                              contentPadding: EdgeInsets.symmetric(
+                                  horizontal: 10, vertical: 2),
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(3),
+                                borderSide: BorderSide(
+                                  width: 0,
+                                  style: BorderStyle.none,
+                                ),
+                              ),
+                              filled: true,
+                              fillColor: Colors.grey.withOpacity(0.25),
+                              focusColor: Colors.grey,
+                            ),
+                          ),
+                        ),
+                        Column(
+                          children: [
+                            ElevatedButton(
+                              onPressed: () async {
+                                setState(() {
+                                  isLoading = true;
+                                });
+                                try {
+                                  print('=============> here');
+                                  final User user = (await _auth
+                                          .createUserWithEmailAndPassword(
+                                              email: email,
+                                              password: password1))
+                                      .user;
+                                  setState(() {
+                                    isLoading = false;
+                                  });
+                                  Navigator.pushNamedAndRemoveUntil(
+                                      context, 'home', (route) => false);
+                                } catch (e) {
+                                  print('==============>>');
+                                  print(e);
+                                  Scaffold.of(context).showSnackBar(
+                                    SnackBar(
+                                      content: Text('Something went wrong'),
+                                    ),
+                                  );
+                                }
+                              },
+                              child: Container(
+                                width: w * 0.45,
+                                child: Center(
+                                  child: Text(
+                                    'Sign Up',
+                                  ),
                                 ),
                               ),
                             ),
-                          ),
-                          Visibility(
-                            child: CircularProgressIndicator(
-                              valueColor: AlwaysStoppedAnimation<Color>(Color(0xFF2F2D52)),
-                            ),
-                            visible: isLoading,
-                          ),
-                        ],
-                      ),
-                      Center(
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            Text("Already have an account?"),
-                            TextButton(
-                              child: Text("Login"),
-                              onPressed: () {
-                                Navigator.of(context).pushNamed('login');
-                              },
+                            Visibility(
+                              child: CircularProgressIndicator(
+                                valueColor: AlwaysStoppedAnimation<Color>(
+                                    Color(0xFF2F2D52)),
+                              ),
+                              visible: isLoading,
                             ),
                           ],
                         ),
-                      )
-                    ],
+                        Center(
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Text("Already have an account?"),
+                              TextButton(
+                                child: Text("Login"),
+                                onPressed: () {
+                                  Navigator.of(context).pushNamed('login');
+                                },
+                              ),
+                            ],
+                          ),
+                        )
+                      ],
+                    ),
                   ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
