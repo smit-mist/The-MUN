6af7d16cfeba32dfe2a8c006e32131c3e5776e0c
smit-mist <smit3355mistry@gmail.com>
2021-01-20 00:10:22 +0530

    ok

--- mun/lib/views/select_city_screen.dart
+++ mun/lib/views/select_city_screen.dart
@@ -42,7 +42,52 @@ class _SelectCityScreenState extends State<SelectCityScreen> {
         padding: EdgeInsets.only(top: 16.0),
         height: h,
         width: w,
-        child: Column(mainAxisSize: MainAxisSize.min, children: []),
+        child: Column(mainAxisSize: MainAxisSize.min, children: [
+          Container(
+            height: h * 0.08,
+            width: w,
+            color: Colors.grey[150],
+            child: TextField(
+              decoration: InputDecoration(
+                prefixIcon: Icon(Icons.search),
+                hintText: 'Search for your city',
+                hintStyle: TextStyle(
+                  fontFamily: 'Helvetica',
+                  color: Colors.black,
+                ),
+              ),
+            ),
+          ),
+          SizedBox(
+            height: h * 0.002,
+          ),
+          Expanded(
+            child: GridView.builder(
+              itemCount: 12,
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 3,
+                mainAxisSpacing: 4.0,
+                crossAxisSpacing: 4.0,
+                //childAspectRatio: (w * 0.04) / (h * 0.008),
+              ),
+              itemBuilder: (BuildContext context, int index) {
+                return Container(
+                  color: Colors.grey[200],
+                  child: Center(
+                    child: Text(
+                      cityNames[index],
+                      style: TextStyle(
+                        fontFamily: 'Helvetica',
+                        color: Colors.black,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                );
+              },
+            ),
+          ),
+        ]),
       ),
     );
   }
