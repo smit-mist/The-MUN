99f63ca88b00310ecb8fa31024792dda0a5c2d1a
smit-mist <smit3355mistry@gmail.com>
2021-02-05 23:11:04 +0530

    Phase 1 Scroll View Added

--- mun/lib/views/booking_mun/about_mun_screen.dart
+++ mun/lib/views/booking_mun/about_mun_screen.dart
@@ -69,154 +69,156 @@ class _AboutMunScreenState extends State<AboutMunScreen> {
       body: Container(
         color: Colors.white,
         padding: EdgeInsets.only(top: 30.0),
-        child: Column(
-          children: [
-            Container(
-              height: h * 0.3,
-              width: w,
-              child: PageView(
-                controller: _controller,
-                children: [
-                  Container(
-                    color: Colors.grey[200],
-                    margin: EdgeInsets.symmetric(horizontal: 12.0),
-                    width: w,
-                    height: h * 0.3,
-                    child: Video(
-                      controller: VideoPlayerController.network(
-                          widget.currentMun.imageUrls[0].toString()),
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Container(
+                height: h * 0.3,
+                width: w,
+                child: PageView(
+                  controller: _controller,
+                  children: [
+                    Container(
+                      color: Colors.grey[200],
+                      margin: EdgeInsets.symmetric(horizontal: 12.0),
+                      width: w,
+                      height: h * 0.3,
+                      child: Video(
+                        controller: VideoPlayerController.network(
+                            widget.currentMun.imageUrls[0].toString()),
+                      ),
                     ),
-                  ),
-                  Container(
-                    child: Image.network(
-                      widget.currentMun.imageUrls[1].toString(),
-                      loadingBuilder: (BuildContext context, Widget child,
-                          ImageChunkEvent loadingProgress) {
-                        if (loadingProgress == null) return child;
-                        return Center(
-                          child: CircularProgressIndicator(
-                            value: loadingProgress.expectedTotalBytes != null
-                                ? loadingProgress.cumulativeBytesLoaded /
-                                    loadingProgress.expectedTotalBytes
-                                : null,
-                          ),
-                        );
-                      },
-                      fit: BoxFit.cover,
+                    Container(
+                      child: Image.network(
+                        widget.currentMun.imageUrls[1].toString(),
+                        loadingBuilder: (BuildContext context, Widget child,
+                            ImageChunkEvent loadingProgress) {
+                          if (loadingProgress == null) return child;
+                          return Center(
+                            child: CircularProgressIndicator(
+                              value: loadingProgress.expectedTotalBytes != null
+                                  ? loadingProgress.cumulativeBytesLoaded /
+                                      loadingProgress.expectedTotalBytes
+                                  : null,
+                            ),
+                          );
+                        },
+                        fit: BoxFit.cover,
+                      ),
                     ),
-                  ),
-                  Container(
-                    child: Image.network(
-                      'https://picsum.photos/id/100/200/300',
-                      loadingBuilder: (BuildContext context, Widget child,
-                          ImageChunkEvent loadingProgress) {
-                        if (loadingProgress == null) return child;
-                        return Center(
-                          child: CircularProgressIndicator(
-                            value: loadingProgress.expectedTotalBytes != null
-                                ? loadingProgress.cumulativeBytesLoaded /
-                                    loadingProgress.expectedTotalBytes
-                                : null,
-                          ),
-                        );
-                      },
-                      fit: BoxFit.cover,
+                    Container(
+                      child: Image.network(
+                        'https://picsum.photos/id/100/200/300',
+                        loadingBuilder: (BuildContext context, Widget child,
+                            ImageChunkEvent loadingProgress) {
+                          if (loadingProgress == null) return child;
+                          return Center(
+                            child: CircularProgressIndicator(
+                              value: loadingProgress.expectedTotalBytes != null
+                                  ? loadingProgress.cumulativeBytesLoaded /
+                                      loadingProgress.expectedTotalBytes
+                                  : null,
+                            ),
+                          );
+                        },
+                        fit: BoxFit.cover,
+                      ),
                     ),
-                  ),
-                ],
+                  ],
+                ),
               ),
-            ),
-            SizedBox(
-              height: h * 0.05,
-            ),
-            Center(
-              child: Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 10.0),
-                child: SingleChildScrollView(
+              SizedBox(
+                height: h * 0.05,
+              ),
+              Center(
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 10.0),
+                  child: SingleChildScrollView(
 
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
 
-                    children: [
-                      Text(
-                        'Venue :- ${widget.currentMun.venue}',
-                        style: simple(16),
-                      ),
-                      SizedBox(
-                        height: 10,
-                      ),
-                      Text(
-                        'Total Seats :- ${widget.currentMun.remainingSeats}',
-                        style: simple(16),
-                      ),
-                      SizedBox(
-                        height: 10,
-                      ),
-                      Text(
-                        'Date :- ${widget.currentMun.date}',
-                        style: simple(16),
-                      ),
-                      SizedBox(
-                        height: 10,
-                      ),
-                      Text(
-                        'Registration Starts :- ${widget.currentMun.registrationTime}',
-                        style: simple(16),
-                      ),
-                      SizedBox(
-                        height: 10,
-                      ),
-                      Text(
-                        'Fees :- ${widget.currentMun.registrationFees}',
-                        style: simple(16),
-                      ),
-                      SizedBox(
-                        height: 10,
-                      ),
-                      Text(
-                        'Description',
+                      children: [
+                        Text(
+                          'Venue :- ${widget.currentMun.venue}',
+                          style: simple(16),
+                        ),
+                        SizedBox(
+                          height: 10,
+                        ),
+                        Text(
+                          'Total Seats :- ${widget.currentMun.remainingSeats}',
+                          style: simple(16),
+                        ),
+                        SizedBox(
+                          height: 10,
+                        ),
+                        Text(
+                          'Date :- ${widget.currentMun.date}',
+                          style: simple(16),
+                        ),
+                        SizedBox(
+                          height: 10,
+                        ),
+                        Text(
+                          'Registration Starts :- ${widget.currentMun.registrationTime}',
+                          style: simple(16),
+                        ),
+                        SizedBox(
+                          height: 10,
+                        ),
+                        Text(
+                          'Fees :- ${widget.currentMun.registrationFees}',
+                          style: simple(16),
+                        ),
+                        SizedBox(
+                          height: 10,
+                        ),
+                        Text(
+                          'Description',
+                          style: boldHeading,
+                        ),
+                        SizedBox(
+                          height: 10,
+                        ),
+                        Text(
+                          widget.currentMun.description,
+                          style: simple(14),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(height: 20,),
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 10),
+                child: Container(
+                  color: Colors.grey[200],
+                  height: h * 0.08,
+                  child: Center(
+                    child: TextButton(
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => SelectCommitteeScreen(
+                              currentMun: widget.currentMun,
+                            ),
+                          ),
+                        );
+                      },
+                      child: Text(
+                        'Select Committee',
                         style: boldHeading,
                       ),
-                      SizedBox(
-                        height: 10,
-                      ),
-                      Text(
-                        widget.currentMun.description,
-                        style: simple(14),
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-            ),
-            SizedBox(height: 20,),
-            Padding(
-              padding: EdgeInsets.symmetric(horizontal: 10),
-              child: Container(
-                color: Colors.grey[200],
-                height: h * 0.08,
-                child: Center(
-                  child: TextButton(
-                    onPressed: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => SelectCommitteeScreen(
-                            currentMun: widget.currentMun,
-                          ),
-                        ),
-                      );
-                    },
-                    child: Text(
-                      'Select Committee',
-                      style: boldHeading,
                     ),
                   ),
                 ),
               ),
-            ),
-            //   SizedBox(height: 10,),
-          ],
+              //   SizedBox(height: 10,),
+            ],
+          ),
         ),
       ),
     );
