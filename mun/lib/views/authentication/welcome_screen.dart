879ad08d76ec556bc79eea97209e6436cb392a69
yashj6294 <56912613+yashj6294@users.noreply.github.com>
2021-05-26 10:36:18 +0530

    Welcome Screen Done

--- mun/lib/views/authentication/welcome_screen.dart
+++ mun/lib/views/authentication/welcome_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:mun/views/elements/constants.dart';
 import 'package:mun/views/elements/textstyles.dart';
+
 class WelcomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -11,117 +12,115 @@ class WelcomeScreen extends StatelessWidget {
         height: h,
         width: w,
         color: Colors.white,
-        child: Center(
+        child: SingleChildScrollView(
+          physics: NeverScrollableScrollPhysics(),
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Container(
-                height: h * 0.4,
-                width: w,
-                child: Center(
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      RichText(
-                        text: TextSpan(
-                            text: 'My',
-                            style: TextStyle(
-                              color: Colors.black,
-                              fontSize: 34,
-                            ),
-                            children: <TextSpan>[
-                              TextSpan(
-                                text: 'Next',
-                                style: TextStyle(
-                                  fontStyle: FontStyle.italic,
-                                  fontWeight: FontWeight.w300,
-                                ),
-                              ),
-                              TextSpan(
-                                  text: 'MUN',
-                                  style: TextStyle(
-                                    fontWeight: FontWeight.bold,
-                                  )),
-                            ]),
-                      ),
-                      SizedBox(
-                        height: h * 0.005,
-                      ),
-                      Text(
-                        'Learn, Book, Participate',
+              SizedBox(
+                height: h * (144 / kScreenHeight),
+              ),
+              RichText(
+                text: TextSpan(
+                    text: 'My',
+                    style: TextStyle(
+                      color: kBlueShade,
+                      fontSize: 34,
+                    ),
+                    children: <TextSpan>[
+                      TextSpan(
+                        text: 'Next',
                         style: TextStyle(
-                          color: Color(0xFFB2B2B2),
-                          fontSize: 15.0,
+                          color: kBlueShade,
+                          fontStyle: FontStyle.italic,
+                          fontWeight: FontWeight.w300,
                         ),
                       ),
-                    ],
+                      TextSpan(
+                          text: 'MUN',
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontWeight: FontWeight.bold,
+                          )),
+                    ]),
+              ),
+              SizedBox(
+                height: 2.0,
+              ),
+              Text(
+                'Learn, Book, Participate',
+                style: robotoRegular(
+                  size: 14.0,
+                  color: Color(0xffB2B2B2),
+                ),
+              ),
+              SizedBox(
+                height: h * (448 / kScreenHeight),
+              ),
+              Container(
+                height: h * (36 / kScreenHeight),
+                width: w * (157 / kScreenWidth),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(3.0),
+                ),
+                child: ElevatedButton(
+                  style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all(kBlueShade),
+                  ),
+                  onPressed: () {
+                    Navigator.pushNamed(context, 'sign_up');
+                  },
+                  child: Text(
+                    'Sign Up',
+                    style: robotoRegular(
+                      size: 20.0,
+                      color: Colors.white,
+                    ).copyWith(
+                      fontWeight: FontWeight.w400,
+                    ),
                   ),
                 ),
               ),
               SizedBox(
-                height: h * 0.35,
+                height: 13.0,
               ),
               Container(
-                height: h * 0.25,
-                width: w,
-                color: Colors.white,
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Container(
-                      height: h * 0.05,
-                      width: w * 0.4,
-                      child: ElevatedButton(
-                        style: ButtonStyle(
-                          backgroundColor:
-                              MaterialStateProperty.all(kBlueShade),
-                        ),
-                        onPressed: () {
-                          Navigator.pushNamed(context, 'sign_up');
-                        },
-                        child: Text(
-                          'Sign Up',
-                          style: robotoMedium(size:18).copyWith(
-                              color: Colors.white, fontWeight: FontWeight.w400),
+                height: h * (36 / kScreenHeight),
+                width: w * (157 / kScreenWidth),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(3.0),
+                  border: Border.all(color: kBlueShade),
+                ),
+                child: ElevatedButton(
+                  style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all(Colors.white),
+                    shape: MaterialStateProperty.all(
+                      BeveledRectangleBorder(
+                        side: BorderSide(
+                          color: kBlueShade,
+                          width: 0.4,
+                          style: BorderStyle.solid,
                         ),
                       ),
                     ),
-                    SizedBox(
-                      height: h * 0.01,
+                  ),
+                  onPressed: () {
+                    Navigator.pushNamed(context, 'login');
+                  },
+                  child: Text(
+                    'Log In',
+                    style: robotoLight(
+                      size: 20,
+                      color: kBlueShade,
+                    ).copyWith(
+                      fontWeight: FontWeight.w300,
                     ),
-                    Container(
-                      height: h * 0.05,
-                      width: w * 0.4,
-                      decoration: BoxDecoration(
-                        border: Border.all(color: kBlueShade),
-                      ),
-                      child: ElevatedButton(
-                        style: ButtonStyle(
-                          backgroundColor:
-                              MaterialStateProperty.all(Colors.white),
-                          shape: MaterialStateProperty.all(
-                            BeveledRectangleBorder(
-                              side: BorderSide(
-                                color: kBlueShade,
-                                width: 0.4,
-                                style: BorderStyle.solid,
-                              ),
-                            ),
-                          ),
-                        ),
-                        onPressed: () {
-                          Navigator.pushNamed(context, 'login');
-                        },
-                        child: Text(
-                          'Log In',
-                          style: robotoMedium(size:18).copyWith(
-                              color: kBlueShade, fontWeight: FontWeight.w400),
-                        ),
-                      ),
-                    ),
-                  ],
+                  ),
                 ),
               ),
+              SizedBox(
+                height: h * (88 / kScreenHeight),
+              )
             ],
           ),
         ),
